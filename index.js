fce133ef71a848066245ced61e8bb82736b0cea6
Yan Kozlovskiy <yankozlovskiy@icloud.com>
2016-09-05 23:39:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function iterativeLog(array) {
+  array.forEach(callback)
+  function callback(element, index, array) {
+    console.log(`${index}: ${element}`);
+  }
+}
+
+function iterate() {
+  
+}
